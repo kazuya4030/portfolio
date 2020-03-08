f28260d039f6f96907e37b5614d4cf3515a411a3
kazuya  uemura <narakazuya0302@gmail.com>
2020-03-08 22:34:26 +0900

    portfolio4030

---
+++ portfolio4030/cafe tokyo/js/main.js
@@ -0,0 +1,15 @@
+$('.hover').hide(0).fadeIn(3000)
+$('.page-title').hide(0).fadeIn(4000)
+$('.margin-bottom').hide(0).fadeIn(5000)
+$('.button').hide(0).fadeIn(6000)
+
+
+
+
+
+
+
+
+
+// ------ NEWS ---------//
+$('.news-title').hide().fadeIn(5000)
